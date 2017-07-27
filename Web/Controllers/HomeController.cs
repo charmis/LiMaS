3d7ea592974453a3841d8258c9989f2853a7cfe5
Charmis Varghese <charmis@users.noreply.github.com>
2017-07-27 15:53:25 -0400

    Added HomeController

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return Ok("Hello World from a controller");
+        }
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server;
 
 namespace Web
 {
@@ -16,6 +17,7 @@ namespace Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ namespace Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
